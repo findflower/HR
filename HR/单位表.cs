5e565ea70eac25ab5e18550533c425df1ef49e21
findflower <zhaom@shinetechchina.com>
2015-11-23 19:11:12 +0800

    Init

---
+++ HR/单位表.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace Data.Models
+{
+    public class 单位表 : BaseModel
+    {
+        public int 上级ID { get; set; }
+        public string 代码 { get; set; }
+        public string 名称 { get; set; }
+        public string 拼音 { get; set; }
+    }
+}
